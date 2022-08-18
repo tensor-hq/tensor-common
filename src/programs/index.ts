93feac5548234115a0038b4677d0e02655b72630
Richard Wu <richardwu1997@gmail.com>
2022-08-18 14:00:12 -0400

    Export ME stuff.

--- src/programs/index.ts
+++ src/programs/index.ts
@@ -1,3 +1,4 @@
 export * from './yawww';
 export * from './auction_house';
 export * from './opensea';
+export * from './magiceden';
--- src/programs/magiceden/index.ts
+++ src/programs/magiceden/index.ts
@@ -3,5 +3,6 @@ export * from './accept_bid';
 export * from './bid';
 export * from './buy';
 export * from './cancel_bid';
+export * from './delist';
 export * from './fund';
 export * from './list';
