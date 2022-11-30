52e51f1ec16854f3b63c33deef3dc8bd4e1c5577
Richard Wu <richardwu1997@gmail.com>
2022-11-29 19:31:29 -0500

    Need to filter by prop name.

--- src/solana_contrib/connection.ts
+++ src/solana_contrib/connection.ts
@@ -1,15 +1,103 @@
 import { Connection } from '@solana/web3.js';
 import { rejectAfterDelay, TimeoutError } from '../utils';
 
+export const FAILOVER_ASYNC_METHODS = [
+  'getBalanceAndContext',
+  'getBalance',
+  'getBlockTime',
+  'getMinimumLedgerSlot',
+  'getFirstAvailableBlock',
+  'getSupply',
+  'getTokenSupply',
+  'getTokenAccountBalance',
+  'getTokenAccountsByOwner',
+  'getParsedTokenAccountsByOwner',
+  'getLargestAccounts',
+  'getTokenLargestAccounts',
+  'getAccountInfoAndContext',
+  'getParsedAccountInfo',
+  'getAccountInfo',
+  'getMultipleAccountsInfoAndContext',
+  'getMultipleAccountsInfo',
+  'getStakeActivation',
+  'getProgramAccounts',
+  'getParsedProgramAccounts',
+  'confirmTransaction',
+  'confirmTransaction',
+  'getClusterNodes',
+  'getVoteAccounts',
+  'getSlot',
+  'getSlotLeader',
+  'getSlotLeaders',
+  'getSignatureStatus',
+  'getSignatureStatuses',
+  'getTransactionCount',
+  'getTotalSupply',
+  'getInflationGovernor',
+  'getInflationReward',
+  'getEpochInfo',
+  'getEpochSchedule',
+  'getLeaderSchedule',
+  'getMinimumBalanceForRentExemption',
+  'getRecentBlockhashAndContext',
+  'getRecentPerformanceSamples',
+  'getFeeCalculatorForBlockhash',
+  'getFeeForMessage',
+  'getRecentBlockhash',
+  'getLatestBlockhash',
+  'getLatestBlockhashAndContext',
+  'getVersion',
+  'getGenesisHash',
+  'getBlock',
+  'getBlock',
+  'getBlockHeight',
+  'getBlockProduction',
+  'getTransaction',
+  'getTransaction',
+  'getParsedTransaction',
+  'getParsedTransactions',
+  'getTransactions',
+  'getTransactions',
+  'getConfirmedBlock',
+  'getBlocks',
+  'getBlockSignatures',
+  'getConfirmedBlockSignatures',
+  'getConfirmedTransaction',
+  'getParsedConfirmedTransaction',
+  'getParsedConfirmedTransactions',
+  'getConfirmedSignaturesForAddress',
+  'getConfirmedSignaturesForAddress2',
+  'getSignaturesForAddress',
+  'getAddressLookupTable',
+  'getNonceAndContext',
+  'getNonce',
+  'requestAirdrop',
+  'getStakeMinimumDelegation',
+  'simulateTransaction',
+  'simulateTransaction',
+  'sendTransaction',
+  'sendTransaction',
+  'sendRawTransaction',
+  'sendEncodedTransaction',
+];
+
 /// This will failover from connection 0..N-1 if an ECONNREFUSED/503/timeout error is encountered.
 export const makeFailoverConnection = (
   conns: Connection[],
-  options?: { timeoutMS?: number },
+  options?: { timeoutMS?: number; failoverAsyncMethods?: string[] },
 ): Connection => {
+  if (!conns.length)
+    throw new Error('require at least 1 connection for failover');
+
   const timeoutMS = options?.timeoutMS;
+  const methods = options?.failoverAsyncMethods ?? FAILOVER_ASYNC_METHODS;
 
   const handler: ProxyHandler<Connection> = {
-    get: (_target, prop, _receiver) => {
+    get: (target, prop, receiver) => {
+      if (!methods.includes(prop.toString())) {
+        return Reflect.get(target, prop, receiver);
+      }
+
       // NB: can't be arrow function.
       return async function () {
         for (const [idx, conn] of conns.entries()) {
