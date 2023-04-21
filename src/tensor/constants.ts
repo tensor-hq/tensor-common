24dfb53b7a5e38ad5ac4d1eed3bde3029473a32b
ilmoi <iljamoi@protonmail.com>
2023-04-21 18:41:35 -0400

    add protocol constants

---
+++ src/tensor/constants.ts
@@ -0,0 +1,50 @@
+import { PublicKey } from '@solana/web3.js';
+
+// (!) KEEP IN SYNC WIHT RESPECTIVE PROTOCOLS
+
+// --------------------------------------- tswap
+
+export const TENSORSWAP_ADDR = new PublicKey(
+  'TSWAPaqyCSx2KABk68Shruf4rp7CxcNi8hAsbdwmHbN',
+);
+
+//@Deprecated, not used inside of Tswap anymore
+export const TSWAP_FEE_ACC = new PublicKey(
+  '4zdNGgAtFsW1cQgHqkiWyRsxaAgxrSRRynnuunxzjxue',
+);
+
+export const TSWAP_COSIGNER = new PublicKey(
+  '6WQvG9Z6D1NZM76Ljz3WjgR7gGXRBJohHASdQxXyKi8q',
+);
+
+export const TSWAP_OWNER = new PublicKey(
+  '99cmWwQMqMFzMPx85rvZYKwusGSjZUDsu6mqYV4iisiz',
+);
+
+export const TSWAP_CORE_LUT = new PublicKey(
+  '9Ses4wW9Mj3nemRJmcZgF81RgLENC6yGUFiMQBPDp5Uk',
+);
+
+// --------------------------------------- tbid
+
+export const TBID_ADDR = new PublicKey(
+  'TB1Dqt8JeKQh7RLDzfYDJsq8KS4fS2yt87avRjyRxMv',
+);
+
+// --------------------------------------- tlist
+
+export const TLIST_ADDR = new PublicKey(
+  'TL1ST2iRBzuGTqLn1KXnGdSnEow62BzPnGiqyRXhWtW',
+);
+
+export const TLIST_COSIGNER = new PublicKey(
+  '5aB7nyNJTuQZdKnhZXQHNhT16tBNevCuLRp14btvANxu',
+);
+
+export const TLIST_OWNER = new PublicKey(
+  '99cmWwQMqMFzMPx85rvZYKwusGSjZUDsu6mqYV4iisiz',
+);
+
+// --------------------------------------- tcomp
+
+//TODO
--- src/tensor/index.ts
+++ src/tensor/index.ts
@@ -2,3 +2,4 @@ export * from './active_listings';
 export * from './nft';
 export * from './traits';
 export * from './types';
+export * from './constants';
