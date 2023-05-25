538d74bddf49f101c76e15a2319827f5274359e2
Richard Wu <richardwu1997@gmail.com>
2023-05-25 10:24:33 -0400

    v0.12.1: fix ME buy args.

--- src/programs/magiceden/buy.ts
+++ src/programs/magiceden/buy.ts
@@ -1,41 +1,32 @@
-import { makeMEHeaders, METxSigned, ME_AH_ADDRESS, ME_URL } from './shared';
-import Big from 'big.js';
-import { LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 import axios from 'axios';
-import { getAssociatedTokenAddress } from '@solana/spl-token';
+import Big from 'big.js';
+import { METxSigned, ME_AH_ADDRESS, ME_URL, makeMEHeaders } from './shared';
 
 export const makeMEBuyTx = async ({
   tokenMint,
+  tokenAcc,
   buyer,
   seller,
   priceLamports,
   apiKey,
+  sellerReferral,
   expiry,
 }: {
   tokenMint: string;
+  tokenAcc: string;
   buyer: string;
   seller: string;
   priceLamports: string;
   apiKey: string;
+  sellerReferral?: string;
   expiry?: number;
-}): Promise<METxSigned> => {
+}): Promise<{
+  txSigned: METxSigned;
+  v0TxSigned?: METxSigned;
+  lastValidBlockHeight: number;
+}> => {
   const price = new Big(priceLamports).div(LAMPORTS_PER_SOL).toNumber();
-
-  const [tokenAccount, sellerReferral] = await Promise.all([
-    getAssociatedTokenAddress(new PublicKey(tokenMint), new PublicKey(seller)),
-    await axios
-      .get<{ sellerReferral?: string; seller: string }[]>(
-        `${ME_URL}/v2/tokens/${tokenMint}/listings`,
-      )
-      .then(
-        (res) =>
-          // If cannot find by seller, fallback to first listing (probably correct).
-          (res.data.find((l) => l.seller === seller) ?? res.data.at(0))
-            ?.sellerReferral,
-      )
-      .catch((_err) => undefined),
-  ]);
-  console.debug(`found referral ${sellerReferral}`);
 
   const { data } = await axios({
     url: `${ME_URL}/v2/instructions/buy_now`,
@@ -45,7 +36,7 @@ export const makeMEBuyTx = async ({
       seller,
       auctionHouseAddress: ME_AH_ADDRESS,
       tokenMint,
-      tokenATA: tokenAccount.toBase58(),
+      tokenATA: tokenAcc,
       price,
       sellerReferral,
       sellerExpiry: expiry,
@@ -53,5 +44,9 @@ export const makeMEBuyTx = async ({
     headers: makeMEHeaders(apiKey),
   });
 
-  return data.txSigned.data;
+  return {
+    txSigned: data.txSigned.data,
+    v0TxSigned: data.v0?.txSigned.data,
+    lastValidBlockHeight: data.blockhashData.lastValidBlockHeight,
+  };
 };
