af95b18ff88d730c04ad6a22df7c0365ca5d8fb5
Richard Wu <richardwu1997@gmail.com>
2023-09-03 22:56:03 -0400

    v0.18.2: added num to buffer conversions for program ixs.

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
-# Tensor Common package
+# Tensor Common
 
-We place all things that needs to be shared b/w front-end & backend.
-
-This means we should be very choosey what goes here and only include stuff that makes sense to include everywhere.
-
-## Development
-
-Make sure to ugprade version in `package.json` for new changes that may be breaking.
-
-### Add to another project
+## Installation
 
 ```sh
-yarn add tensor-hq/tensor-common
+npm i @tensor-hq/tensor-common
+yarn add @tensor-hq/tensor-common
 ```
--- src/solana_contrib/utils.ts
+++ src/solana_contrib/utils.ts
@@ -19,3 +19,40 @@ export const getAccountRent = (
 // based on https://docs.solana.com/developing/programming-model/accounts#:~:text=The%20current%20maximum%20size%20of,per%20account%20and%20per%20instruction.
 export const getAccountRentSync = (dataSize: number) =>
   Math.trunc(19.055441478439427 * (128 + dataSize) * 365.25);
+
+export const numToi32Bytes = (num: number) => {
+  // Ensure number is a 32-bit signed integer
+  if (num > 2147483647 || num < -2147483648) {
+    throw new Error('Number out of range for i32');
+  }
+
+  const bytes = new Uint8Array(4);
+  for (let i = 0; i < 4; i++) {
+    // Take the lowest 8 bits of the number and store them in the current byte
+    bytes[i] = num & 0xff;
+    // Right-shift the number by 8 bits to prepare for the next byte extraction
+    num >>= 8;
+  }
+  return bytes;
+};
+
+export const numToU64Bytes = (num: number) => {
+  const buffer = new ArrayBuffer(8);
+  const view = new DataView(buffer);
+  view.setBigUint64(0, BigInt(num), true); // true for little endian
+  return new Uint8Array(buffer);
+};
+
+export const numToU32Bytes = (num: number) => {
+  const buffer = new ArrayBuffer(4);
+  const view = new DataView(buffer);
+  view.setUint32(0, num, true); // true for little endian
+  return new Uint8Array(buffer);
+};
+
+export const numToU8Bytes = (num: number) => {
+  const buffer = new ArrayBuffer(1);
+  const view = new DataView(buffer);
+  view.setUint8(0, num);
+  return new Uint8Array(buffer);
+};
