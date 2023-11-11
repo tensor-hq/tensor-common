ee86412d09047fdd7f6141a92b758138b2886a38
avoc-ado <avoc-ado@users.noreply.github.com>
2023-11-11 14:31:26 -0800

    remove token-auth-rules

--- src/metaplex/pdas/token_rules.ts
+++ src/metaplex/pdas/token_rules.ts
@@ -1,11 +1,12 @@
 import { PublicKey } from '@solana/web3.js';
 export { PROGRAM_ID as TMETA_PROG_ID } from '@metaplex-foundation/mpl-token-metadata';
-export { PROGRAM_ID as AUTH_PROG_ID } from '@metaplex-foundation/mpl-token-auth-rules';
 import { PROGRAM_ID as TMETA_PROG_ID } from '@metaplex-foundation/mpl-token-metadata';
-import {
-  PREFIX,
-  PROGRAM_ID as AUTH_PROG_ID,
-} from '@metaplex-foundation/mpl-token-auth-rules';
+
+export const AUTH_PROG_ID = new PublicKey(
+  'auth9SigNpDKz4sJJ1DfCTuZrZNSAgh9sFD3rboVmgg',
+);
+
+const PREFIX = 'rule_set';
 
 export const findTokenRecordPda = (mint: PublicKey, token: PublicKey) => {
   return PublicKey.findProgramAddressSync(
