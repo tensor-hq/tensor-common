ce01e0792c89a005adcec155ba42df6e9fc91eab
avoc-ado <avoc-ado@users.noreply.github.com>
2023-11-11 13:30:36 -0800

    get leafe asset id sync

--- src/metaplex/pdas/bubblegum.ts
+++ src/metaplex/pdas/bubblegum.ts
@@ -1,4 +1,5 @@
 import { PublicKey } from '@solana/web3.js';
+import BN from 'bn.js';
 
 export const BUBBLEGUM_PROGRAM_ID = new PublicKey(
   'BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY',
@@ -26,3 +27,19 @@ export const findBubblegumMintAuthorityPda = (
 ) => {
   return PublicKey.findProgramAddressSync([mint.toBytes()], programId);
 };
+
+export function getLeafAssetId(
+  tree: PublicKey,
+  leafIndex: BN,
+  programId: PublicKey = BUBBLEGUM_PROGRAM_ID,
+): PublicKey {
+  const [assetId] = PublicKey.findProgramAddressSync(
+    [
+      Buffer.from('asset', 'utf8'),
+      tree.toBuffer(),
+      Uint8Array.from(leafIndex.toArray('le', 8)),
+    ],
+    programId,
+  );
+  return assetId;
+}
