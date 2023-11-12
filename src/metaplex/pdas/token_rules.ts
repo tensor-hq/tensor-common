158a8c80ee71473ff7a0785a55925897777fa844
avoc-ado <avoc-ado@users.noreply.github.com>
2023-11-12 10:30:08 -0800

    use local program id

--- src/metaplex/pdas/token_rules.ts
+++ src/metaplex/pdas/token_rules.ts
@@ -1,6 +1,5 @@
 import { PublicKey } from '@solana/web3.js';
-export { PROGRAM_ID as TMETA_PROG_ID } from '@metaplex-foundation/mpl-token-metadata';
-import { PROGRAM_ID as TMETA_PROG_ID } from '@metaplex-foundation/mpl-token-metadata';
+import { TOKEN_METADATA_PROGRAM_ID } from 'src/programs/auction_house/shared';
 
 export const AUTH_PROG_ID = new PublicKey(
   'auth9SigNpDKz4sJJ1DfCTuZrZNSAgh9sFD3rboVmgg',
@@ -12,12 +11,12 @@ export const findTokenRecordPda = (mint: PublicKey, token: PublicKey) => {
   return PublicKey.findProgramAddressSync(
     [
       Buffer.from('metadata'),
-      TMETA_PROG_ID.toBuffer(),
+      TOKEN_METADATA_PROGRAM_ID.toBuffer(),
       mint.toBuffer(),
       Buffer.from('token_record'),
       token.toBuffer(),
     ],
-    TMETA_PROG_ID,
+    TOKEN_METADATA_PROGRAM_ID,
   );
 };
 
