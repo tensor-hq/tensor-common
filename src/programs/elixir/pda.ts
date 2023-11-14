a8a3e0cb7ced155efb15f2cb976f6469915b8567
avoc-ado <avoc-ado@users.noreply.github.com>
2023-11-14 10:29:30 -0800

    remove anchor for utf-8 encode

--- src/programs/elixir/pda.ts
+++ src/programs/elixir/pda.ts
@@ -1,17 +1,16 @@
 import { PublicKey } from '@solana/web3.js';
-import { utils } from '@coral-xyz/anchor';
 import { ELIXIR_PROGRAM_IDS } from './constants';
 
 export const findVaultAccPda = (fnftMint: PublicKey) => {
   return PublicKey.findProgramAddressSync(
-    [Buffer.from(utils.bytes.utf8.encode('vault')), fnftMint.toBytes()],
+    [Buffer.from('vault'), fnftMint.toBytes()],
     ELIXIR_PROGRAM_IDS.vault,
   );
 };
 
 export const findFeeAccPda = (poolMint: PublicKey) => {
   return PublicKey.findProgramAddressSync(
-    [Buffer.from(utils.bytes.utf8.encode('deposit')), poolMint.toBytes()],
+    [Buffer.from('deposit'), poolMint.toBytes()],
     ELIXIR_PROGRAM_IDS.vault,
   );
 };
@@ -21,28 +20,21 @@ export const findAppraisalAccPda = (
   nftMint: PublicKey,
 ) => {
   return PublicKey.findProgramAddressSync(
-    [
-      Buffer.from(utils.bytes.utf8.encode('appraisal')),
-      poolMint.toBytes(),
-      nftMint.toBytes(),
-    ],
+    [Buffer.from('appraisal'), poolMint.toBytes(), nftMint.toBytes()],
     ELIXIR_PROGRAM_IDS.vault,
   );
 };
 
 export const findExternalAccPda = (poolMint: PublicKey) => {
   return PublicKey.findProgramAddressSync(
-    [
-      Buffer.from(utils.bytes.utf8.encode('fractions-seed')),
-      poolMint.toBytes(),
-    ],
+    [Buffer.from('fractions-seed'), poolMint.toBytes()],
     ELIXIR_PROGRAM_IDS.vault,
   );
 };
 
 export const findPoolAccPda = (poolMint: PublicKey) => {
   return PublicKey.findProgramAddressSync(
-    [Buffer.from(utils.bytes.utf8.encode('fractions')), poolMint.toBytes()],
+    [Buffer.from('fractions'), poolMint.toBytes()],
     ELIXIR_PROGRAM_IDS.vault,
   );
 };
