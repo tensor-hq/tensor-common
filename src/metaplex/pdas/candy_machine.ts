f647aed3863ef548356a62a8e02057c1910be9b4
avoc-ado <135168569+avoc-ado@users.noreply.github.com>
2024-02-16 23:19:32 -0500

    v5.2.0 candy machine lut

---
+++ src/metaplex/pdas/candy_machine.ts
@@ -0,0 +1,8 @@
+import { PublicKey } from '@solana/web3.js';
+
+export const CANDY_GUARD_PROGRAM_ID = new PublicKey(
+  'Guard1JwRhJkVH6XZhzoYxeBVQe872VH6QggF4BWmS9g',
+);
+export const CANDY_V3_PROGRAM_ID = new PublicKey(
+  'CndyV3LdqHUfDLmE5naZjVN8rBZz4tqhdefbAnjHG3JR',
+);
--- src/metaplex/pdas/index.ts
+++ src/metaplex/pdas/index.ts
@@ -1,4 +1,5 @@
 export * from './auction_house';
 export * from './bubblegum';
+export * from './candy_machine';
 export * from './token_metadata';
 export * from './token_rules';
