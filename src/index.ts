03ef845104fcf1a5561eb5a411d96db47a2257cb
Alex Wormuth <alex@tensor.so>
2024-03-27 22:45:31 -0500

    add metaplex core program id (#47)
    * add mpl core program id
    
    * bump package.json

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,5 @@ export * from './tensor/lambdas';
 export * as test_utils from './test_utils';
 export * from './time';
 export * from './token22';
+export * from './mpl_core';
 export * from './utils';
---
+++ src/mpl_core/mpl_core.ts
@@ -0,0 +1,5 @@
+import { PublicKey } from '@solana/web3.js';
+
+export const MPL_CORE_PROGRAM_ID = new PublicKey(
+  'CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d',
+);
