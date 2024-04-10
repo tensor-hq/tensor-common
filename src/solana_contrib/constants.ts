0ff52c7ada2032f216bdd9564077ceca9874957f
avoc-ado <135168569+avoc-ado@users.noreply.github.com>
2024-04-10 22:24:49 +0800

    Revert "add COMPUTE_BUDGET_PROGRAM_ID"
    This reverts commit a23b4822a01fc7567d870ab15421f2ce9e4d6c4c.

--- src/solana_contrib/constants.ts
+++
@@ -1,6 +0,0 @@
-import { PublicKey } from '@solana/web3.js';
-
-// greatly smaller bundle than using ComputeBudget.programId
-export const COMPUTE_BUDGET_PROGRAM_ID = new PublicKey(
-  'ComputeBudget111111111111111111111111111111',
-);
--- src/solana_contrib/index.ts
+++ src/solana_contrib/index.ts
@@ -1,6 +1,5 @@
 export * from './anchor';
 export * from './connection';
-export * from './constants';
 export * from './lut';
 export * from './spl_token';
 export * from './transaction';
