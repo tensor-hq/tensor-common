7ede0f11c3cbd119d03d9338d566bce7e780620c
Sam Devorsetz <samdevorsetz@gmail.com>
2025-04-04 16:15:20 -0400

    [ts-utils] add weeks and months to time constants

--- src/time.ts
+++ src/time.ts
@@ -2,6 +2,8 @@ export const SECONDS = 1000;
 export const MINUTES = 60 * SECONDS;
 export const HOURS = 60 * MINUTES;
 export const DAYS = 24 * HOURS;
+export const WEEKS = 7 * DAYS;
+export const MONTHS = 30 * DAYS;
 export const YEARS = 365.25 * DAYS;
 
 export interface Timespan {
